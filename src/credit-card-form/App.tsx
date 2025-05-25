c7b9ff09632ba724ff5431439eb6393565f15cde
Martín Mármol <martinmarmol.ar@gmail.com>
2025-05-25 18:38:43 -0300

    refactor: standardize code formatting and indentation across project files

--- src/credit-card-form/App.tsx
+++ src/credit-card-form/App.tsx
@@ -1,7 +1,7 @@
 export default function CreditCardApp() {
-  return (
-    <div>
-      <h1>Credit Card Form</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Credit Card Form</h1>
+		</div>
+	);
 }
