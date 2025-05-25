2ea9cf94c3abd1290314e2c914077704ca299f54
Martín Mármol <martinmarmol.ar@gmail.com>
2025-05-25 19:24:58 -0300

    chore: update package.json to add lint-staged and husky for improved code quality

--- eslint.config.js
+++ eslint.config.js
@@ -3,18 +3,19 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
-	{ ignores: ["dist"] },
+	{ ignores: ["dist", "build", "node_modules"] },
 	{
 		extends: [
 			js.configs.recommended,
 			...tseslint.configs.recommended,
-			"prettier",
+			prettier,
 		],
 		files: ["**/*.{ts,tsx}"],
 		languageOptions: {
-			ecmaVersion: 2020,
+			ecmaVersion: "latest",
 			globals: globals.browser,
 		},
 		plugins: {
@@ -27,6 +28,8 @@ export default tseslint.config(
 				"warn",
 				{ allowConstantExport: true },
 			],
+			"no-console": "warn",
+			indent: ["error", "tab"],
 		},
-	}
+	},
 );
